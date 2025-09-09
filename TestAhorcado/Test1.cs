ad5ced2f7085471a20c6bb09991e05f9139955c0
felifernandez07 <fmf123fmf@gmail.com>
2025-09-08 21:49:29 -0300

    test tirar y acertar letra

---
+++ TestAhorcado/Test1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace Ahorcado
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestTirarLetraEYAcertarTrue()
+        {
+            var juego = new TPAhorcado.Program();
+            string letra = "e";
+
+            bool resultado = juego.TirarLetraYAcertar(letra);
+
+            Assert.IsTrue(resultado);
+        }
+
+        
+        
+    }
+}
